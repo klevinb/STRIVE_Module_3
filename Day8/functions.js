82298b78993271b57319a5189c2aa12150137229
klevinb <klevin.bazaiti98@gmail.com>
2020-05-27 13:17:50 +0200

    Displaying products from server to index

--- Day8/functions.js
+++ Day8/functions.js
@@ -3,10 +3,20 @@ const user = "user16"
 const password = "c9WEUxMS294hN6fF"
 const headers = new Headers()
 headers.set('Authorization', 'Basic ' + btoa(user + ":" + password))
+headers.set("Content-Type", "application/json")
 
 getData = async () =>{
     let response = await fetch(url,{
         headers,
     })
     return await response.json()
+}
+
+sendProduct = async (product) =>{
+    let response = await fetch(url, {
+        method: "POST",
+        body: JSON.stringify(product),
+        headers: headers
+      });
+    return response
 }
